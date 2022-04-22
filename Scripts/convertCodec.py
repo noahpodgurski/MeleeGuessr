bdc2bbca579a612618ce402c54f3880a8ec5023c
Noah <noahpodgurski@gmail.com>
2022-04-21 21:38:10 -0400

    updated scripts to handle slp2clip better

--- Scripts/convertCodec.py
+++ Scripts/convertCodec.py
@@ -12,11 +12,11 @@ def getCommand(inFile, outFile):
 def ish264(file):
 	command = f"{FFPROBE} -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 {file}"
 	result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-	print(result.returncode, result.stdout, result.stderr)
+	# print(result.returncode, result.stdout, result.stderr)
 	return result.stdout.strip("\n") == "h264"
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 	# testIn = "C:/Users/Noah/Documents/Programming/Websites/MeleeGuessr/Scripts/out/clip0.mp4"
 	# testOut = "C:/Users/Noah/Documents/Programming/Websites/MeleeGuessr/Scripts/out/clip0converted.mp4"
 	# convert(testIn, testOut)
--- Scripts/createClipsJson.py
+++ Scripts/createClipsJson.py
@@ -1,56 +1,70 @@
-import convertSlptToClip
+import os
+import json
+import logging
+import threading
 
+import convertSlpToClip
 from splitMang0Highlights import OUT_PATH
 import convertCodec
 
 def createClipsJson(data, count):
     print("creating clips.json...")
     clips = []
-    for i, highlight in enumerate(data['queue']):
-        
+    x = input("Do you want to rerender missing clips? ")
+    rerender = False
+    if x == "y":
+        rerender = True
         missingFiles = []
-        if not os.path.exists(f"{OUT_PATH}/clip{i}.mp4" or not os.path.exists(f"{OUT_PATH}/neutclip{i}.mp4"):
-            print(f"neut(?)clip{i} is missing, rerendering...")
-            missingFiles.append(i)
-        if not convertCodec.ish264(f"{OUT_PATH}/clip{i}.mp4") or not convertCodec.ish264(f"{OUT_PATH}/neutclip{i}.mp4")):
-            print(f"neut(?)clip{i} is incorrect codec, rerendering...")
-            missingFiles.append(i)
+    for i, highlight in enumerate(data['queue']):
+        if rerender:
+            if not os.path.exists(f"{OUT_PATH}/clip{i}.mp4") or not os.path.exists(f"{OUT_PATH}/neutclip{i}.mp4"):
+                print(f"neut(?)clip{i} is missing, rerendering...")
+                missingFiles.append(i)
+            if not convertCodec.ish264(f"{OUT_PATH}/clip{i}.mp4") or not convertCodec.ish264(f"{OUT_PATH}/neutclip{i}.mp4"):
+                print(f"neut(?)clip{i} is incorrect codec, rerendering...")
+                missingFiles.append(i)
+
+            numThreads = 4
+
+            def thread_func(thread_num):
+                for i in range(thread_num, len(missingFiles), numThreads):
+                    convertSlpToClip.convertSlpToClip(missingFiles[i], count)
 
 
+            threads = list()
+            for index in range(numThreads):
+                logging.info("Main    : create and start thread %d.", index)
+                x = threading.Thread(target=thread_func, args=(index,))
+                threads.append(x)
+                x.start()
 
-        numThreads = 4
-
-        def thread_func(thread_num):
-            for i in range(thread_num, len(missingFiles), numThreads):
-                convertSlptToClip.convertSlptToClip(missingFiles[i], count)
-
-
-        threads = list()
-        for index in range(numThreads):
-            logging.info("Main    : create and start thread %d.", index)
-            x = threading.Thread(target=thread_func, args=(index,))
-            threads.append(x)
-            x.start()
-
-        for index, thread in enumerate(threads):
-            logging.info("Main    : before joining thread %d.", index)
-            thread.join()
-            logging.info("Main    : thread %d done", index)
+            for index, thread in enumerate(threads):
+                logging.info("Main    : before joining thread %d.", index)
+                thread.join()
+                logging.info("Main    : thread %d done", index)
 
 
         if os.path.exists(f"{OUT_PATH}/clip{i}.mp4") and os.path.exists(f"{OUT_PATH}/neutclip{i}.mp4") and convertCodec.ish264(f"{OUT_PATH}/clip{i}.mp4") and convertCodec.ish264(f"{OUT_PATH}/neutclip{i}.mp4"):
+            if not highlight.get('character'):
+                print(f"{i} doesn't have a character... skipping")
+                continue
             clips.append({
                 "clipSrc": f"clip{i}",
                 "neutclipSrc": f"neutclip{i}",
                 "slp": highlight['path'],
                 'gameStation': highlight.get('gameStation'),
                 "character": highlight['character'],
+                'oppChar': highlight.get('oppChar'),
                 'player': highlight.get('player'),
+                'oppPlayer': highlight.get('oppPlayer')
             })
 
     #get past player data
-    with open(f"{OUT_PATH}/server/clips.json", "r") as f:
-        oldClips = json.load(f)
+    try:
+        with open(f"{OUT_PATH}/clips.json", "r") as f:
+            oldClips = json.load(f)
+    except:
+        oldClips = []
 
     #populate with past player data
     for i, clip in enumerate(oldClips):
