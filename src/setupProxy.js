0ccdb69dc651d16749c5be2f287f1fe214a9cedc
Noah <noahpodgurski@gmail.com>
2022-04-26 18:39:40 -0400

    small changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,23 +1,12 @@
-// const { SERVER_IP } = require('../config');
-// const SERVER_IP = 'http://localhost:4000/';
-const SERVER_IP = 'http://44.204.159.79:4000/';
+const BUCKET_IP = 'https://meleeguessr-clips.s3.amazonaws.com/';
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
   app.use(
-    ['/clips'],
-    createProxyMiddleware({
-      target: SERVER_IP,
-      changeOrigin: true
-    })
-  );
-
-
-  app.use(
     ['/videos/*'],
     createProxyMiddleware({
-      target: 'https://meleeguessr-clips.s3.amazonaws.com/',
+      target: BUCKET_IP,
       changeOrigin: true,
-      pathRewrite: (req) => { console.log(`REQ: ${req}`); return req.substring(7)}
+      pathRewrite: (req) => { return `${req.substring(8)}`} //removes /videos/
     })
   );
 };
