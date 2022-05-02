a05512b3768cc56a8e2630d1c4e2cebc6253e7bd
Noah <noahpodgurski@gmail.com>
2022-05-02 12:48:59 -0400

    video no proxy

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -217,11 +217,11 @@ export const Stage = forwardRef((props: StageProps, ref: Ref<RefObject>) => {
     return ( 
       <>
         <video ref={neutclipRef} key={`neut${stage.clipSrc}`} preload="auto" className={`clip`} loop muted playsInline>
-          <source src={`/videos/neut${stage.clipSrc}.mp4`} type="video/mp4" />
+          <source src={`https://meleeguessr-clips.s3.amazonaws.com/neut${stage.clipSrc}.mp4`} type="video/mp4" />
         </video>
         <video ref={clipRef} key={stage.clipSrc} className={`clip`} preload="auto" hidden loop playsInline>
         {/* <video ref={clipRef} key={stage.clipSrc} className={`clip`} hidden loop playsInline> */}
-          <source src={`/videos/${stage.clipSrc}.mp4`} type="video/mp4" />
+          <source src={`https://meleeguessr-clips.s3.amazonaws.com/${stage.clipSrc}.mp4`} type="video/mp4" />
         </video>
       </>
     )
