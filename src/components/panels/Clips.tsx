d262e80195f92ba1a569072c7851845e87c39eda
Noah <noahpodgurski@gmail.com>
2024-08-22 13:59:42 -0400

    add question marks

--- src/components/panels/Clips.tsx
+++ src/components/panels/Clips.tsx
@@ -3,9 +3,6 @@ import { PlayerBadge } from "~/components/common/Badge";
 import { Highlight } from "~/search/search";
 import {
   replayStore,
-  selectCustomAction,
-  selectCustomAttack,
-  selectHighlight,
 } from "~/state/replayStore";
 import * as accordion from "@zag-js/accordion";
 import { normalizeProps, useMachine } from "@zag-js/solid";
@@ -48,43 +45,6 @@ export function Clips() {
                     name !== "customAttack",
                 })}
               >
-                {name === "customAction" ? (
-                  <>
-                    Action:
-                    <select
-                      class="min-w-0 shrink border"
-                      value={replayStore.customAction}
-                      onChange={(e) =>
-                        selectCustomAction(
-                          (e.target as HTMLSelectElement).value as ActionName
-                        )
-                      }
-                    >
-                      <For each={actionNameById}>
-                        {(action) => <option value={action}>{action}</option>}
-                      </For>
-                    </select>
-                  </>
-                ) : name === "customAttack" ? (
-                  <>
-                    Attack:
-                    <select
-                      class="min-w-0 shrink border"
-                      value={replayStore.customAttack}
-                      onChange={(e) =>
-                        selectCustomAttack(
-                          (e.target as HTMLSelectElement).value as AttackName
-                        )
-                      }
-                    >
-                      <For each={attackNamesById}>
-                        {(attack) => <option value={attack}>{attack}</option>}
-                      </For>
-                    </select>
-                  </>
-                ) : (
-                  name
-                )}
                 <Show when={highlights.length > 0}>
                   <div class="material-icons">
                     {api().getItemState({ value: name }).expanded
@@ -94,42 +54,9 @@ export function Clips() {
                 </Show>
               </button>
             </h3>
-            <div class="mb-5" {...api().getItemContentProps({ value: name })}>
-              <For each={highlights}>
-                {(highlight) => <ClipRow name={name} highlight={highlight} />}
-              </For>
-            </div>
           </div>
         )}
       </For>
     </div>
-  );
-}
-
-function ClipRow(props: { name: string; highlight: Highlight }) {
-  return (
-    <>
-      <div
-        class="flex w-full cursor-pointer items-center overflow-hidden whitespace-nowrap border p-1 hover:bg-slate-100"
-        classList={{
-          "bg-slate-200 hover:bg-slate-300":
-            replayStore.selectedHighlight?.[0] === props.name &&
-            replayStore.selectedHighlight?.[1].startFrame ===
-              props.highlight.startFrame &&
-            replayStore.selectedHighlight?.[1].endFrame ===
-              props.highlight.endFrame &&
-            replayStore.selectedHighlight?.[1].playerIndex ===
-              props.highlight.playerIndex,
-        }}
-        onClick={() => selectHighlight([props.name, props.highlight])}
-      >
-        <div class="flex items-center gap-1">
-          <PlayerBadge port={props.highlight.playerIndex + 1} />
-        </div>
-        <div class="flex flex-grow items-center justify-center">
-          {props.highlight.startFrame}-{props.highlight.endFrame}
-        </div>
-      </div>
-    </>
   );
 }
