7cc97c330eb07f749d3a8a58e8d5dfe11428c498
Noah <noahpodgurski@gmail.com>
2024-09-04 12:26:03 -0400

    include otf

--- src/App.tsx
+++ src/App.tsx
@@ -23,30 +23,6 @@ import { Loader } from "./components/Loader";
 import { loadUser } from "./state/userStore";
 export const STARTING_STOCKS = 4;
 
-interface ICount {
-  [key: string]: number;
-}
-
-const findAlias = (player: string) => {
-  for (let m in Player) {
-    if (m.toLowerCase() === player.toLowerCase()) {
-      return m;
-    }
-    if (Player[m].aliases) {
-      let aliases = Player[m].aliases || [];
-      for (let alias in aliases) {
-        if (aliases[alias].toLowerCase() === player.toLowerCase()) return m;
-      }
-    }
-  }
-  return null;
-}
-
-// Create contexts
-const StocksContext = createContext();
-const ClipsContext = createContext();
-const UserContext = createContext();
-
 export const App = () => {
   const [stocks, setStocks] = createSignal(STARTING_STOCKS);
   const [clips, setClips] = createSignal<Clip[]>([]);
@@ -71,62 +47,6 @@ export const App = () => {
     } catch (err) {
       setUser(null);
     }
-  };
-
-  const getClips = async () => {
-    // if (clips().length === 0) {
-    //   const _clips = res;
-    //   const fixedClips: Clip[] = _clips.map((clip: any) => {
-    //     if (!Player[clip['player']]) {
-    //       let _player = findAlias(clip['player']);
-    //       if (!_player) {
-    //         clip['player'] = 'TEST';
-    //       } else {
-    //         clip['player'] = _player;
-    //       }
-    //     }
-
-    //     return {
-    //       ...clip,
-    //       slp: clip.slp,
-    //       character: CharacterIds[clip.character],
-    //       player: Player[clip['player']],
-    //       oppChar: CharacterIds[clip.oppChar],
-    //       oppPlayer: clip['oppPlayer']
-    //     };
-    //   }) || [];
-
-    //   const counts: ICount = {};
-    //   fixedClips.forEach((clip: Clip) => {
-    //     if (counts[clip.player.label]) {
-    //       counts[clip.player.label] += 1;
-    //     } else {
-    //       counts[clip.player.label] = 1;
-    //     }
-    //   });
-
-    //   let avg = 0;
-    //   let count = 0;
-    //   for (let player in counts) {
-    //     avg += counts[player];
-    //     count++;
-    //   }
-    //   avg /= count;
-
-    //   const filteredCounts: ICount = {};
-    //   const filteredClips = fixedClips.filter((clip: Clip) => {
-    //     if (!filteredCounts[clip.player.label]) {
-    //       filteredCounts[clip.player.label] = 1;
-    //       return true;
-    //     }
-    //     if (filteredCounts[clip.player.label] < avg) {
-    //       filteredCounts[clip.player.label] += 1;
-    //       return true;
-    //     }
-    //     return false;
-    //   });
-    //   setClips(filteredClips);
-    // }
   };
 
   const navTheme = createTheme({
--- vite.config.mts
+++ vite.config.mts
@@ -3,7 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  assetsInclude: [/.*zip$/, /.*ttf$/],
+  assetsInclude: [/.*zip$/, /.*ttf$/, /.*otf$/],
   plugins: [solidPlugin(), tsConfigPaths()],
   resolve: {
     conditions: ["browser"],
